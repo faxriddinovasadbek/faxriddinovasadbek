ceb0b43b907927b72107a95f436bc1e2c92f2ef8
faxriddinovasadbek <153736237+faxriddinovasadbek@users.noreply.github.com>
2023-12-26 15:56:33 +0500

    Create leetcode_334.go

---
+++ leetcode_334.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func reverseString(s []byte) {
+	
+	for i, j := 0 , len(s)-1;  i < j ; i,j = i+1, j-1{
+		s[i], s[j] = s[j], s[i]
+	}
+
+	fmt.Println(string(s))
+}
+
+func main(){
+	n := []byte{'h','e','l','l','o'}
+	reverseString(n)
+}
